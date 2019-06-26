768e26e80e46bdf3a6060cc9d5ba3a8cc29f8eaf
paatap <paatap@gmail.com>
2019-06-26 19:31:35 +0400

    version Until Upload

---
+++ src/sendemailtlsprops/SendEmailTLSProps.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sendemailtlsprops;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
+ 
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+/**
+ *
+ * @author paatap
+ */
+public class SendEmailTLSProps {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String host = "smtp.gmail.com";
+        String port = "587";
+        String mailFrom = "info@compare.ge";
+        String password = "Gelashvili2019";
+ 
+        // message info
+        String mailTo = "sarchizt@gmail.com";
+        String subject = "compare.ge Information Center";
+        String message = "ნახეთ ფაილები/I have some attachments for you.";
+ 
+        // attachments
+        String[] attachFiles = new String[3];
+        attachFiles[0] = "/home/paatap/Desktop/61664582.pdf";
+        attachFiles[1] = "/home/paatap/Desktop/47575817_370944463678988_2550836370503368704_n.png";
+        attachFiles[2] = "/home/paatap/Desktop/1111.jpg";
+ 
+        try {
+            sendEmailWithAttachments(host, port, mailFrom, password, mailTo,
+                subject, message, attachFiles);
+            System.out.println("Email sent.");
+        } catch (Exception ex) {
+            System.out.println("Could not send email.");
+            ex.printStackTrace();
+        }
+    }
+        
+          public static void sendEmailWithAttachments(String host, String port,
+            final String userName, final String password, String toAddress,
+            String subject, String message, String[] attachFiles)
+            throws AddressException, MessagingException {
+        // sets SMTP server properties
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", port);
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.user", userName);
+        properties.put("mail.password", password);
+ 
+        // creates a new session with an authenticator
+        Authenticator auth = new Authenticator() {
+            public PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(userName, password);
+            }
+        };
+        Session session = Session.getInstance(properties, auth);
+ 
+        // creates a new e-mail message
+        Message msg = new MimeMessage(session);
+ 
+        msg.setFrom(new InternetAddress(userName));
+        InternetAddress[] toAddresses = { new InternetAddress(toAddress) };
+        msg.setRecipients(Message.RecipientType.TO, toAddresses);
+        msg.setSubject(subject);
+        msg.setSentDate(new Date());
+ 
+        // creates message part
+        MimeBodyPart messageBodyPart = new MimeBodyPart();
+        messageBodyPart.setContent(message, "text/html");
+ 
+        // creates multi-part
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(messageBodyPart);
+ 
+        // adds attachments
+        if (attachFiles != null && attachFiles.length > 0) {
+            for (String filePath : attachFiles) {
+                MimeBodyPart attachPart = new MimeBodyPart();
+ 
+                try {
+                    attachPart.attachFile(filePath);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+ 
+                multipart.addBodyPart(attachPart);
+            }
+        }
+ 
+        // sets the multi-part as e-mail's content
+        msg.setContent(multipart);
+ 
+        // sends the e-mail
+        Transport.send(msg);
+ 
+    }  
+    
+    
+}
